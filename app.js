62b487da1aa57d4e3b57d1b578d7f40a74fdffd0
Hammami Salmen <salmen.hammami@sfax.r-iset.tn>
2026-05-10 12:10:12 +0100

    Modification du texte 1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello DevOps  !!!!");
+  res.send("Hello DevOps  !");
 });
 
 // Connexion au serveur
